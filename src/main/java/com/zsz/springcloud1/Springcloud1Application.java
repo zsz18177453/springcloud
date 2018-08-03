c70787627b66ef796c4e2c37f370692ebdbd1b08
朱顺忠 <zhushunzhong@chaoxing.com>
2018-08-03 15:17:31 +0800

    dd

---
+++ src/main/java/com/zsz/springcloud1/Springcloud1Application.java
@@ -0,0 +1,12 @@
+package com.zsz.springcloud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcloud1Application.class, args);
+	}
+}
